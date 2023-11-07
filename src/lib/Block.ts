a5e754b19cc83f108c0ecf3ddd63233d357d1f64
Dmitriy Kiselev <sckdv1988@gmail.com>
2023-11-07 17:19:24 +0300

    upd

--- src/lib/Block.ts
+++ src/lib/Block.ts
@@ -4,17 +4,20 @@ class Block {
     static EVENTS = {
         INIT: "init",
         FLOW_CDM: "flow:component-did-mount",
+        FLOW_CDU: "flow:component-did-update",
         FLOW_RENDER: "flow:render"
     };
     _element = null;
     _meta = null;
 
+    // @ts-ignore
     /** JSDoc
      * @param {string} tagName
      * @param {Object} props
      *
      * @returns {void}
      */
+    // @ts-ignore
     constructor(tagName: string = 'div', props: object = {}): void {
         const eventBus = new EventBus();
 
@@ -34,6 +37,7 @@ class Block {
     _registerEvents(eventBus: EventBus) {
         eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
         eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+        eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
         eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
     }
 
@@ -44,7 +48,7 @@ class Block {
 
     init() {
         this._createResources();
-        this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+        this._render();
     }
 
     _componentDidMount() {
@@ -52,25 +56,27 @@ class Block {
         this.componentDidMount();
     }
 
-    componentDidMount(oldProps) {}
+    componentDidMount() {}
 
     dispatchComponentDidMount() {
         this._eventBus().emit(Block.EVENTS.FLOW_CDM);
     }
 
-    _componentDidUpdate(oldProps, newProps) {
-    // ...
-    }
 
+    _componentDidUpdate(oldProps: any, newProps: any) {
+        if (this.componentDidUpdate(oldProps, newProps)) {
+            this.eventBus().emit(Block.EVENTS.FLOW_RENDER)
+        }
+        const response = this.componentDidUpdate(oldProps, newProps);
+    }
+    // Может переопределять пользователь, необязательно трогать
     componentDidUpdate(oldProps, newProps) {
         return true;
     }
-
     setProps = (nextProps: any) => {
         if (!nextProps) {
             return;
         }
-
         Object.assign(this.props, nextProps);
     };
 
@@ -84,6 +90,7 @@ class Block {
         // Используйте шаблонизатор из npm или напишите свой безопасный
         // Нужно компилировать не в строку (или делать это правильно),
         // либо сразу превращать в DOM-элементы и возвращать из compile DOM-ноду
+        // @ts-ignore
         this._element.innerHTML = block;
     }
 
@@ -94,24 +101,41 @@ class Block {
         return this.element;
     }
 
-    _makePropsProxy(props) {
+    _makePropsProxy(props: object) {
         // Ещё один способ передачи this, но он больше не применяется с приходом ES6+
         const self = this;
-
-        // Здесь вам предстоит реализовать метод
+        props = new Proxy(props, {
+            get(target, prop) {
+                // @ts-ignore
+                const value = target[prop];
+                console.log("get data: ", value);
+                return typeof value === "function" ? value.bind(target) : value;
+            },
+            set(target, prop, value) {
+                // @ts-ignore
+                target[prop] = value;
+                self.eventBus().emit(Block.EVENTS.FLOW_CDU, [self.props, target])
+                return true;
+            },
+            deleteProperty() {
+                throw new Error("Нет доступа")
+            }
+        });
         return props;
     }
 
-    _createDocumentElement(tagName) {
+    _createDocumentElement(tagName: any) {
         // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
         return document.createElement(tagName);
     }
 
     show() {
+        // @ts-ignore
         this.getContent().style.display = "block";
     }
 
     hide() {
+        // @ts-ignore
         this.getContent().style.display = "none";
     }
 }
