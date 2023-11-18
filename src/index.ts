ebcfece2c3d52893095c93a267f13a174b30e9f7
Dmitriy Kiselev <sckdv1988@gmail.com>
2023-11-18 17:24:30 +0300

    upd new deploy ver. 2.5

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ import ButtonBack from './components/buttonBack';
 import ProfileEdit from './pages/profile-edit';
 import ProfilePwdEdit from './pages/password-edit';
 import ModalsPage from './pages/modals';
-import Block from './lib/Block.ts';
 
 
 const authPage = new MainLayout(
@@ -79,92 +78,59 @@ const profilePwdEdit = new ProfileLayout(
 
 const modalsPage = new ModalsPage()
 
-// const ROUTES = {
-//   auth: '/',
-//   signin: '/signin',
-//   err404: '/err404',
-//   err500: '/err500',
-//   chat: '/chat',
-//   profile: '/profile',
-//   profileEdit: '/profile-edit',
-//   profilePwdEdit: '/password-edit',
-//   modals: '/modals'
-// }
-
-
-// const changeRoute = (): any => {
-//   let path = window.location.pathname
-//   if (path === ROUTES.auth) {
-//     return  authPage
-//   }
-//   if (path === ROUTES.signin) {
-//     return signinPage
-//   }
-//   if (path === ROUTES.err404) {
-//     return error404
-//   }
-//   if (path === ROUTES.err500) {
-//     return error500
-//   }
-//   if (path === ROUTES.chat) {
-//     return chatPage
-//   }
-//   if (path === ROUTES.profile) {
-//     return profilePage
-//   }
-//   if (path === ROUTES.profileEdit) {
-//     return profileEdit
-//   }
-//   if (path === ROUTES.profilePwdEdit) {
-//     return profilePwdEdit
-//   }
-//   if (path === ROUTES.modals) {
-//     return modalsPage
-//   }
-// }
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   const root = document.getElementById('app');
-//
-//   if (root) {
-//     const component: MainLayout = changeRoute()
-//     root.append(component._element!);
-//     component.dispatchComponentDidMount();
-//   }
-// });
-
-const ROUTES2 = {
-    '/': authPage,
-    '/signin': signinPage,
-    '/err404': error404,
-    '/err500': error500,
-    '/chat': chatPage,
-    '/profile': profilePage,
-    '/profile-edit': profileEdit,
-    '/password-edit': profilePwdEdit,
-    '/modals': modalsPage
+const ROUTES = {
+  auth: '/',
+  signin: '/signin',
+  err404: '/err404',
+  err500: '/err500',
+  chat: '/chat',
+  profile: '/profile',
+  profileEdit: '/profile-edit',
+  profilePwdEdit: '/password-edit',
+  modals: '/modals'
 }
 
-export const renderDOM = (query: string, block: Block) => {
-    const root = document.querySelector(query);
 
-    if (!root) {
-        throw new Error(`Элемент ${query} не найден`);
-    }
+const changeRoute = (): any => {
+  let path = window.location.pathname
+  if (path === ROUTES.auth) {
+    return  authPage
+  }
+  if (path === ROUTES.signin) {
+    return signinPage
+  }
+  if (path === ROUTES.err404) {
+    return error404
+  }
+  if (path === ROUTES.err500) {
+    return error500
+  }
+  if (path === ROUTES.chat) {
+    return chatPage
+  }
+  if (path === ROUTES.profile) {
+    return profilePage
+  }
+  if (path === ROUTES.profileEdit) {
+    return profileEdit
+  }
+  if (path === ROUTES.profilePwdEdit) {
+    return profilePwdEdit
+  }
+  if (path === ROUTES.modals) {
+    return modalsPage
+  }
+}
 
-    if (!block.getContent()) {
-        return root;
-    }
 
-    root.appendChild(block.getContent() as HTMLElement);
-
-    block.dispatchComponentDidMount();
-
-    return root;
-};
 
 window.addEventListener('DOMContentLoaded', () => {
-    const path = window.location.pathname as keyof typeof ROUTES2;
-    renderDOM('#app', ROUTES2[path]);
+  const root = document.getElementById('app');
+
+  if (root) {
+    const component: MainLayout = changeRoute()
+    root.append(component._element!);
+    component.dispatchComponentDidMount();
+  }
 });
+
